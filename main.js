800bef3831477506001d442bd24c11a0b48cce97
Christopher Clark <102836432+chrisclarkzip@users.noreply.github.com>
2022-04-24 01:19:11 -0700

    quick update

--- main.js
+++ main.js
@@ -1,11 +1,26 @@
-let clickCount = 0 
+let clickCount = 0
+let height = 120
+let width = 100
+let inflationRate = 20
+let maxsize = 300
+let popCount = 0
 
-function inflate(){
+function inflate() {
     clickCount++
-    var balloonElement = document.getElementById("balloon")
-   balloonElement.style.height = "400px"
-   balloonElement.style.width = "300px"
+    let balloonElement = document.getElementById("balloon")
+    height += inflationRate
+    width += inflationRate
+    if(height >= maxsize){
+        console.log("pop the balloon")
+        popCount++
+        height = 0
+        width = 0
+        document.getElementById('pop-count').innerText = popCount.toString()
+    }
 
-   var clickCountElem = document.getElementById("click-count")
-   clickCountElem.innerText = clickCount.toString()
+    balloonElement.style.height = height + "px"
+    balloonElement.style.width = width + "px"
+
+    let clickCountElem = document.getElementById("click-count")
+    clickCountElem.innerText = clickCount.toString()
 }
