e6f886025ccb985e4a4a81622b5f530048a3de4d
Christopher Clark <102836432+chrisclarkzip@users.noreply.github.com>
2022-04-24 22:01:55 -0700

    quick update

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+let startButton = document.getElementById('start-button')
+let inflateButton = document.getElementById('inflate-button')
+
 let clickCount = 0
 let height = 120
 let width = 100
@@ -5,11 +8,27 @@ let inflationRate = 20
 let maxsize = 300
 let popCount = 0
 
+function startGame(){
+
+    startButton.setAttribute("disabled", "true")
+    inflateButton.removeAttribute("disabled")
+
+    setTimeout(() => {
+        console.log("its been three seconds")
+
+    inflateButton.setAttribute("disabled", "true")
+    startButton.removeAttribute("disabled")
+    
+    
+    }, 3000)
+}
+
 function inflate() {
     clickCount++
     let balloonElement = document.getElementById("balloon")
     height += inflationRate
     width += inflationRate
+
     if(height >= maxsize){
         console.log("pop the balloon")
         popCount++
@@ -17,7 +36,7 @@ function inflate() {
         width = 0
         document.getElementById('pop-count').innerText = popCount.toString()
     }
-
+     
     balloonElement.style.height = height + "px"
     balloonElement.style.width = width + "px"
 
